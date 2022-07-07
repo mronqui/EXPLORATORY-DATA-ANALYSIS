95f7a5ab9b08edf493fefa909a4b1d2e50bac56c
Mauricio Ronqui <64265771+mronqui@users.noreply.github.com>
2022-07-07 17:18:29 -0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+##sh mu_setup.sh
+
+echo"instalando librerias"
+pip install -r requirements.txt
+
---
+++ src/app.py
@@ -0,0 +1,7 @@
+
+#!pip install git+https://github.com/CristianLazoQuispe/datautils.git
+
+from utils import db_connect
+engine = db_connect()
+
+# your code here
